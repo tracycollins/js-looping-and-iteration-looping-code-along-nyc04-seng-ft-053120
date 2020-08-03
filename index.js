d6215cec3adde7ec7c5cd91cff8d3d55f6ed36f6
tracycollins <tc@threeceemedia.com>
2020-08-02 22:56:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function countDown(startingNum) {
+  for (let i = startingNum; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+function writeCards(names, message) {
+  const results = [];
+  for (let i = 0; i < names.length; i++) {
+    results.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`)
+  }
+  return results;
+}
